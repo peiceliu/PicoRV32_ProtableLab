561a60be2f83365ad2a7560cb0b9ae6b11c6831a
zcxzcxzcxzcx <77569318+zcxzcxzcxzcx@users.noreply.github.com>
2024-09-24 14:30:46 +0800

    Add files via upload
    add my_logic_analysis rtl

---
+++ pg50_proj/2_RTL_code/my_logic_analysis/my_logic_analysis.v
@@ -0,0 +1,173 @@
+module my_logic_analysis #(
+    parameter                       INPUT_WIDTH = 2             //  任意分频方式（每次递增INCREASE）的计数器最大位宽，越大则精度越高
+)(  
+    input                           clk                     ,
+    input                           rst_n                   ,
+    // input                           start                   ,
+    input                           config_valid            ,
+    input       [7:0]               config_in               ,
+    input       [INPUT_WIDTH-1:0]   din                     ,                            //  逻辑分析输入数据
+    output      [7:0]               dout                    ,
+    output reg                      fifo_wen                ,
+    input                           fifo_data_full          ,
+    input                           fifo_data_alfull                                        //
+);
+
+reg                                 bps_start               ;
+reg                                 clk_bps                 ;
+reg             [12:0]              bps_ctrl                ;   
+reg                                 clk_bps_r               ;           
+wire                                clk_posedge             ; 
+wire                                clk_negedge             ;
+reg             [7:0]               config_in_r             ;
+reg             [7:0]               dout_r                  ;           
+reg             [3:0]               din_cnt                 ;
+reg             [INPUT_WIDTH-1:0]   din_r                   ;
+
+
+localparam DLY = 1;
+//================================时钟沿产生==================================
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        clk_bps_r <= #DLY 'd0;
+    end else begin
+        clk_bps_r <= #DLY clk_bps;
+    end
+end
+
+assign clk_posedge = clk_bps & ~clk_bps_r;
+assign clk_negedge = ~clk_bps & clk_bps_r;
+
+//=================================
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        config_in_r <= #DLY 'd0;
+    end else begin
+        if (config_valid) begin
+            config_in_r <= #DLY config_in;
+        end
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        bps_ctrl <= #DLY 'd0;
+    end else begin
+        case(config_in_r[6:4])
+            3'd0: begin
+                bps_ctrl <= #DLY 'd0;      
+            end
+            3'd1: begin
+                bps_ctrl <= #DLY 13'd2603;      
+            end
+            3'd2: begin
+                bps_ctrl <= #DLY 13'd1301;       
+            end
+            3'd3: begin
+                bps_ctrl <= #DLY 13'd868;       //57600bps
+            end
+            3'd4: begin
+                bps_ctrl <= #DLY 13'd521;       // 13'd5207,    //9600bps
+            end
+            3'd5: begin
+                bps_ctrl <= #DLY 13'd260;      // 13'd2603,    //19200bps
+            end
+            3'd6: begin
+                bps_ctrl <= #DLY 13'd130;       // 13'd1301,    //38400bps
+            end
+            3'd7: begin
+                bps_ctrl <= #DLY 13'd87;       // 13'd868;     //57600bps
+            end
+            default: begin
+                bps_ctrl <= #DLY 13'd521;
+            end
+        endcase
+    end
+end
+//=====================================
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        din_cnt <= #DLY 'd0;
+    end else begin
+        if (config_in_r[3] && clk_posedge && din_cnt == 'd3) begin
+            din_cnt <= #DLY 'd0;
+        end else if (config_in_r[3] && clk_posedge) begin
+            din_cnt <= #DLY din_cnt + 'd1;
+        end
+        if (~config_in_r[3] && clk_negedge && din_cnt == 'd3) begin
+            din_cnt <= #DLY 'd0;
+        end else if (~config_in_r[3] && clk_negedge) begin
+            din_cnt <= #DLY din_cnt + 'd1;
+        end
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        dout_r <= #DLY 'd0;
+    end else begin
+        if (config_in_r[3] && clk_posedge) begin
+            dout_r[3:0] <= #DLY {dout_r[2:0],din[0]};
+            dout_r[7:4] <= #DLY {dout_r[6:4],din[1]};
+        end
+        if (~config_in_r[3] && clk_negedge) begin
+            dout_r[3:0] <= #DLY {dout_r[2:0],din[0]};
+            dout_r[7:4] <= #DLY {dout_r[6:4],din[1]};
+        end
+    end
+end
+assign dout = dout_r;
+
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        fifo_wen <= #DLY 'd0;
+    end else begin
+        if (fifo_wen) begin
+            fifo_wen <= #DLY 'd0;
+        end else if (config_in_r[3] && clk_posedge && din_cnt == 'd3 && ~fifo_data_full) begin
+            fifo_wen <= #DLY 'd1;
+        end else if (~config_in_r[3] && clk_negedge && din_cnt == 'd3 && ~fifo_data_full) begin
+            fifo_wen <= #DLY 'd1;
+        end
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        bps_start <= 'd0;
+    end else begin
+        if (fifo_data_alfull || config_in_r == 'd0) begin  
+            bps_start <= #DLY 'd0;
+        end else if (config_in_r[0]) begin
+            bps_start <= #DLY 'd1;
+        end else if (din_r != din) begin
+            bps_start <= #DLY 'd1;
+        end
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if(rst_n == 1'b0) begin
+        din_r <= 'd0;
+    end else begin
+        din_r <= din;
+    end
+end
+
+
+clk_div clk_div_1 (
+	.clk            (clk            )   , 
+	.rst_n          (rst_n          )   , 
+	.bps_start      (bps_start      )   , 
+    .uart_ctrl      (bps_ctrl       )   , //baudrates config.
+	.clk_bps        (clk_bps        ) 
+);
+// 13'd5207,    //9600bps
+// 13'd2603,    //19200bps
+// 13'd1301,    //38400bps
+// 13'd867,     //57600bps
+// 13'd433;     //115200bps
+
+
+
+endmodule
---
+++ pg50_proj/2_RTL_code/my_logic_analysis/syn_fifo.v
@@ -0,0 +1,90 @@
+// ========================================================================================================== //
+//                               Copyright 2022 Deepcreatic Technologies Co.Ltd                               //
+//                                            All rights reserved                                             //
+// ---------------------------------------------------------------------------------------------------------- //
+// Module Full Name: SYN_FIFO
+// File Name: syn_fifo.v
+// Author: wycong  @author: wycong0416@gmail.com
+// Create Date: 2022.11.04
+// version: 0.1
+// Function Description: synchronous fifo
+// Modified By: 
+// Modified Date: 
+// Description: 
+// ========================================================================================================== //
+
+module SYN_FIFO(
+clk                   ,
+rst_n                 ,
+wen                   ,
+ren                   ,
+level                 ,
+din                   ,
+dout                  ,
+alfull                ,
+empty                 ,
+full                   
+);
+parameter      DLY           =   1            ;
+parameter      ADDR_WIDTH    =   12            ;
+parameter      DATA_WIDTH    =   8           ;
+
+input                       clk               ;
+input                       rst_n             ;
+input                       wen               ;
+input                       ren               ;
+input   [ADDR_WIDTH:0]      level             ; //the space be occupied.   
+input   [DATA_WIDTH-1:0]    din               ;
+output  [DATA_WIDTH-1:0]    dout              ;
+output                      alfull            ;
+output                      empty             ;
+output                      full              ;
+
+reg     [ADDR_WIDTH:0]      waddr             ;
+reg     [ADDR_WIDTH:0]      raddr             ;
+
+wire                        alfull            ;
+wire                        empty             ;
+wire                        full              ;
+wire    [DATA_WIDTH-1:0]    dout              ;
+
+
+always @(negedge rst_n or posedge clk) begin
+    if(rst_n == 1'b0) begin
+        waddr <= 'h0;
+    end
+    else begin
+        if(wen == 1'b1 && full == 1'b0) begin
+            waddr <= #DLY waddr + 1'b1;
+        end
+    end
+end
+
+always @(negedge rst_n or posedge clk) begin
+    if(rst_n == 1'b0) begin
+        raddr <= 'h0;
+    end
+    else begin
+        if(ren == 1'b1) begin
+            raddr <= #DLY raddr + 1'b1;
+        end
+    end
+end
+
+assign alfull = (waddr - raddr) >= level  ;
+assign empty  = raddr == waddr            ;
+assign full = (waddr[ADDR_WIDTH] != raddr[ADDR_WIDTH]) & (waddr[ADDR_WIDTH-1:0] == raddr[ADDR_WIDTH-1:0]); 
+
+DPRAM_WRAP U_DPRAM_WRAP(
+.wclk      (clk                        ),
+.rclk      (clk                        ),
+.waddr     (waddr[ADDR_WIDTH-1:0]      ),
+.raddr     (raddr[ADDR_WIDTH-1:0]      ),
+.din       (din                        ),
+.wen       (wen & ~full                ),
+.ren       (ren                        ),
+.dout      (dout                       ) 
+);
+
+
+endmodule
